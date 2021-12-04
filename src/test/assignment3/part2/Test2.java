e576bfae4c9f20090ddaafc8ae622e349f109822
ZexinPeng <zexinpeng97@outlook.com>
2021-12-04 12:08:52 -0500

    test: test AVL

--- src/test/assignment3/part2/Test2.java
+++ src/test/assignment3/part2/Test2.java
@@ -3,37 +3,40 @@ package test.assignment3.part2;
 import assignment3.part2.CleverSIDCImpl;
 
 // This class test the function of LinkedList
-public class Test1 {
+public class Test2 {
 
     public static void main(String[] args) {
         CleverSIDCImpl cleverSIDC = new CleverSIDCImpl();
+        cleverSIDC.setSIDCThreshold(1);
         System.out.println("================");
         System.out.println("test insertion");
-        Tool.add(cleverSIDC, 100, Tool.generateValue(100));
-        Tool.add(cleverSIDC, 2330, Tool.generateValue(2330));
-        Tool.add(cleverSIDC, 30, Tool.generateValue(30));
-        Tool.add(cleverSIDC, 1, Tool.generateValue(1));
-        Tool.add(cleverSIDC, 33210, Tool.generateValue(33210));
         Tool.add(cleverSIDC, 10, Tool.generateValue(10));
+        Tool.add(cleverSIDC, 9, Tool.generateValue(9));
         Tool.add(cleverSIDC, 8, Tool.generateValue(8));
-        Tool.add(cleverSIDC, 1000, Tool.generateValue(1000));
+        Tool.add(cleverSIDC, 7, Tool.generateValue(7));
+        Tool.add(cleverSIDC, 6, Tool.generateValue(6));
+        Tool.add(cleverSIDC, 5, Tool.generateValue(5));
+        Tool.add(cleverSIDC, 4, Tool.generateValue(4));
+        Tool.add(cleverSIDC, 3, Tool.generateValue(3));
+        Tool.add(cleverSIDC, 2, Tool.generateValue(2));
+        Tool.add(cleverSIDC, 1, Tool.generateValue(1));
 
         System.out.println("================");
         System.out.println("test getValue");
         Tool.printValue(cleverSIDC, 10);
-        Tool.printValue(cleverSIDC, 8);
-        Tool.printValue(cleverSIDC, 1000);
+        Tool.printValue(cleverSIDC, 5);
+        Tool.printValue(cleverSIDC, 1);
 
         Tool.printKeys(cleverSIDC.allKeys());
 
         System.out.println("==============");
         System.out.println("test remove");
         Tool.remove(cleverSIDC, 10);
-        Tool.remove(cleverSIDC, 8);
-        Tool.remove(cleverSIDC, 1000);
+        Tool.remove(cleverSIDC, 5);
+        Tool.remove(cleverSIDC, 1);
         Tool.printValue(cleverSIDC, 10);
-        Tool.printValue(cleverSIDC, 8);
-        Tool.printValue(cleverSIDC, 1000);
+        Tool.printValue(cleverSIDC, 5);
+        Tool.printValue(cleverSIDC, 1);
 
         Tool.printKeys(cleverSIDC.allKeys());
 
@@ -48,23 +51,23 @@ public class Test1 {
         Tool.printKeys(cleverSIDC.allKeys());
         System.out.println("==============");
         System.out.println("test nextKey");
+        Tool.printNextKey(cleverSIDC, 1);
         Tool.printNextKey(cleverSIDC, 4);
-        Tool.printNextKey(cleverSIDC, 8);
-        Tool.printNextKey(cleverSIDC, 100);
+        Tool.printNextKey(cleverSIDC, 7);
         Tool.printNextKey(cleverSIDC, 33210);
 
         System.out.println("==============");
         System.out.println("test prevKey");
         Tool.printPrevKey(cleverSIDC, 4);
         Tool.printPrevKey(cleverSIDC, 8);
-        Tool.printPrevKey(cleverSIDC, 30);
-        Tool.printPrevKey(cleverSIDC, 1);
+        Tool.printPrevKey(cleverSIDC, 10);
+        Tool.printPrevKey(cleverSIDC, 6);
 
         System.out.println("==============");
         System.out.println("test rangeKey");
         Tool.printRangeKey(cleverSIDC, 1, 2);
-        Tool.printRangeKey(cleverSIDC, 28, 32);
-        Tool.printRangeKey(cleverSIDC, 1, 30);
+        Tool.printRangeKey(cleverSIDC, 4, 6);
+        Tool.printRangeKey(cleverSIDC, 1, 8);
         Tool.printRangeKey(cleverSIDC, 1, (int)Math.pow(10, 8) - 1);
     }
 }
